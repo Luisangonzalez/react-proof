7029b6ca0a998ace4df3035e995ae155a1cf1501
Luis Antonio González Martín <lagonzalez@qdqmedia.com>
2017-06-29 00:56:14 +0200

    add form in login and regiser component

--- src/login/login.js
+++ src/login/login.js
@@ -4,7 +4,19 @@ import { Link } from 'react-router-dom'
 class Login extends React.Component {
   render() {
     return (
+      <div class="qz-register">
           <h1>Login</h1>
+          <form>
+            <label>User/Password</label>
+            <input className="form-control"/>
+            <label>Password</label>
+            <input className="form-control"/>
+            <button type="submit">Register</button>
+          </form>
+          <p> value</p>
+          <p> status</p>
+          <Link to='/'>Home</Link>
+      </div>
     );
   }
 }
--- src/register/register.js
+++ src/register/register.js
@@ -4,7 +4,25 @@ import { Link } from 'react-router-dom'
 class Register extends React.Component {
   render() {
     return (
-          <h1>Register</h1>
+            <div class="qz-register">
+                <h1>Register</h1>
+                <form>
+                  <label>Name</label>
+                  <input className="form-control"/>
+                  <label>Lastname</label>
+                  <input className="form-control"/>
+                  <label>Username</label>
+                  <input className="form-control"/>
+                  <label>Email</label>
+                  <input className="form-control"/>
+                  <label>Password</label>
+                  <input className="form-control"/>
+                  <button type="submit">Register</button>
+                </form>
+                <p> value</p>
+                <p> status</p>
+                <Link to='/'>Home</Link>
+            </div>
     );
   }
 }
